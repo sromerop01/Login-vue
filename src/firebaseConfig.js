43e3b979a232d95226bc7223d5c4bd7026405ffa
Santiago Romero <sromerop2001@gmail.com>
2023-07-23 17:14:13 -0500

    Configuracion Firestore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
 
 
 // Your web app's Firebase configuration
@@ -17,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
-
 const auth = getAuth()
+const db = getFirestore()
 
-export { auth }
+export { auth, db }
