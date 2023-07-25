6c45ee14f7f110cf6a766ac29696f684845520ef
Santiago Romero <sromerop2001@gmail.com>
2023-07-24 23:25:38 -0500

    [ADD] Layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
+<template>
+  <a-layout>
+    <a-layout-header v-if="!useUser.loadingSession">
+      <a-menu
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+        v-model:selectedKeys="selectedKeys"
+      >
+        <a-menu-item v-if="userData.email" key="home">
+          <RouterLink to="/">Home</RouterLink>
+        </a-menu-item>
+        <a-menu-item v-if="!userData.email" key="login">
+          <RouterLink to="/login">login</RouterLink>
+        </a-menu-item>
+        <a-menu-item v-if="!userData.email" key="register">
+          <RouterLink to="/register">Register</RouterLink>
+        </a-menu-item>
+        <a-menu-item v-if="userData.email" @click="logoutUser">
+          Logout
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+        <div v-if="useUser.loadingSession">Loading User...</div>
+        <RouterView/>
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { useUserStore } from './stores/user'
-import { storeToRefs } from 'pinia';
+import { useRoute } from "vue-router";
+import { ref, watch } from 'vue';
 
+const route = useRoute()
 const useUser = useUserStore()
 const { logoutUser } = useUser
 const { userData } = useUser
 
-</script>
+const selectedKeys = ref([])
 
-<template>
-  <a-layout>
-    <a-layout-header v-if="!useUser.loadingSession">
-      <nav>
-        <RouterLink to="/" v-if="userData.email">Home</RouterLink>
-        <RouterLink to="/login" v-if="!userData.email">login</RouterLink>
-        <RouterLink to="/register" v-if="!userData.email">Register</RouterLink>
-        <button @click="logoutUser" v-if="userData.email">Logout</button>
-      </nav>
-    </a-layout-header>
-    <a-layout-content v-if="useUser.loadingSession">
-      <div>
-        Loading User...
-      </div>
-    </a-layout-content>
-    <RouterView />
-  </a-layout>
-</template>
+watch(() => route.name, () => selectedKeys.value = [route.name])
+
+</script>
